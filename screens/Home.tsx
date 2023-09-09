8a5e4be0b9afdc78ba54b73f9d58583dcfbec24f
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-09-09 14:54:09 -0400

    Revert ":wqRevert "refactor passing data w route""
    This reverts commit 3bda64edbf0a917e0de64bae3299cb02fa3c7cb8.
    
    reversing what I donked up

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from "react-native";
 import { globalStyles } from "../styles/Global";
+// import { useNavigation } from "@react-navigation/native";
 
-const Home: React.FC = () => {
+type Props = {
+    navigation: any;
+}
+
+const Home: React.FC<Props> = ({ navigation }) => {
+    // const navigation = useNavigation();
+    const [reviews, setReviews] = useState([
+        { title: "Zelda, Breath of Fresh Air", rating: 5, body: "lorem ipsum", key: "1" },
+        { title: "Gotta Catch Them All (again)", rating: 4, body: "lorem ipsum", key: "2" },
+        { title: "Not So Final Fantasy", rating: 3, body: "lorem ipsum", key: "3" },
+    ]);
+
+    const pressHandler = () => {
+        navigation.navigate( "Review Details");
+        //or
+        // navigation.push("Review Details");
+    }
+
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
+      {/* <Text style={globalStyles.titleText}>Home Screen</Text> */}
+      {/* <Button title="Go to Review Dets" onPress={pressHandler} /> */}
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate("Review Details", item)}>
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+        />
     </View>
   );
 }
